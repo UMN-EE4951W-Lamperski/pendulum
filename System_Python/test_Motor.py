1e298b701fbc1c1048122efc5181fb37eeba2829
Raspberry Pi <pi@umn.edu>
2019-09-24 16:57:14 -0500

    Tested on RPi and minor changes to make it work.

--- System_Python/test_Motor.py
+++ System_Python/test_Motor.py
@@ -2,22 +2,22 @@ from motor import Motor
 import time
 
 # Decide which pins to hook up to on the Pi before running
-speed_pin = 15
-forward_pin = 16
-reverse_pin = 17
+speed_pin = 17
+forward_pin = 27
+reverse_pin = 22
 
 m = Motor(speed_pin, forward_pin, reverse_pin)
 
 dir = 'ascending'
 speed = 0.0
 while 1:
-	m.Move(speed)
-	if speed >= 100.0:
-		dir = 'descending'
-	elif speed <= -100.0:
-		dir = 'ascending'
-	if dir == 'ascending':
-		speed = speed + 0.5
-	else:
-		speed = speed - 0.5
-	time.sleep(0.1)
+    m.Move(speed)
+    if speed >= 100.0:
+        dir = 'descending'
+    elif speed <= -100.0:
+        dir = 'ascending'
+    if dir == 'ascending':
+        speed = speed + 0.5
+    else:
+        speed = speed - 0.5
+    time.sleep(0.1)
